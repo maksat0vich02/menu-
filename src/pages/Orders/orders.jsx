f10137fec2c43c62988021d57962b202a693f767
maksat0vich02 <maksat0vich0002@gmail.com>
2024-01-30 11:40:33 +0600

    menu

--- src/pages/Orders/orders.jsx
+++ src/pages/Orders/orders.jsx
@@ -4,8 +4,10 @@ import imgOne from "../../images/order-Img-1.png";
 import imgTwo from "../../images/order-Img-2.png";
 
 const Orders = () => {
-  const [count, setcount] = useState(0);
-  const [plus, setplus] = useState(0);
+  const [count, setcount] = useState(1);
+  const [plus, setplus] = useState(1);
+  const [price, setprice] = useState(5);
+  const [prices, setprices] = useState(12.5);
   function min() {
     if (count > 1) {
       setcount(count - 1);
@@ -27,7 +29,7 @@ const Orders = () => {
             <img src={imgOne} alt="" />
             <div className="text">
               <h3>cappuccino</h3>
-              <p>10.5$</p>
+              <p>{price * count}$</p>
             </div>
             <div className="delete">
               <button className="delBtn">delete order</button>
@@ -56,7 +58,7 @@ const Orders = () => {
             <img src={imgTwo} alt="" />
             <div className="text">
               <h3>sushi and rolls</h3>
-              <p>12.5$</p>
+              <p>{prices * plus}$</p>
             </div>
             <div className="delete">
               <button className="delBtn">delete order</button>
@@ -69,7 +71,7 @@ const Orders = () => {
                 >
                   -
                 </button>
-                <h1>{plus + "x"}</h1>
+                <h1>{plus}x</h1>
                 <button
                   className="pul"
                   onClick={() => {
